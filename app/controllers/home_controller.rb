e1f134a030c88b0882b68231e8ee5e5a5e2329a0
Arnab Deka <arnab.deka@gmail.com>
2014-07-23 13:35:18 +0530

    Hook up form with invoice resource.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def dashboard
     @pre_signed_post = KuluAWS.new.presigned_post
+    @invoice = Invoice.new
+    @invoice.url_prefix = @pre_signed_post.key
   end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,4 @@
 class Invoice
+  include ActiveModel::Model
+  attr_accessor :url_prefix, :filename
 end
