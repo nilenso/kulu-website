066e658f13f292353854aa011588e1e139cb581c
Akshay Gupta <kitallis@gmail.com>
2014-08-02 01:51:28 +0530

    refactoring the create invoice spec to stub the api call

--- spec/controllers/invoices_controller_spec.rb
+++ spec/controllers/invoices_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe InvoicesController, :type => :controller do
-  pending 'POST create' do
+  context 'POST create' do
     it 'creates an invoice resource' do
       expected_filename = 'foo/bar/foo.png'
       expect(KuluService::API).to receive(:create_invoice).with(expected_filename)
@@ -9,6 +9,7 @@ RSpec.describe InvoicesController, :type => :controller do
     end
 
     it 'redirects to the root path' do
+      allow(KuluService::API).to receive(:create_invoice)
       post :create, invoice: { filename: 'foo.png', url_prefix: 'foo/bar/${filename}' }
       expect(response).to redirect_to(root_path)
     end
