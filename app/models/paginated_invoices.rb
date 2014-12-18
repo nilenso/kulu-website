784e653e5ad1648a77d3ee0200da32e71777c2f2
Smit Shah <who828@gmail.com>
2014-12-18 18:50:06 +0530

    Reverse the invoices page order

--- app/models/paginated_invoices.rb
+++ app/models/paginated_invoices.rb
@@ -4,7 +4,7 @@ class PaginatedInvoices < Kaminari::PaginatableArray
   attr_reader :current_page, :total_pages, :limit_value
 
   def initialize(raw_data)
-    invoices = raw_data['items'].map {|i| Invoice.new(i).decorate }
+    invoices = raw_data['items'].map {|i| Invoice.new(i).decorate }.reverse
     @current_page, @limit_value, @total_pages, total_count = raw_data['meta'].values_at(*PAGINATION_KEYS)
     super(invoices, total_count: total_count, limit: limit_value)
   end
