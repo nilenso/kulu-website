a4980e837c845e1f7e6078fb29e6196140b32a3f
Akshay Gupta <kitallis@gmail.com>
2015-01-02 21:48:32 +0530

    hotfix: new api breaks create invoice, hack stubbing the extra parameters for the time being

--- app/models/kulu_service/api.rb
+++ app/models/kulu_service/api.rb
@@ -9,7 +9,15 @@ module KuluService
     end
 
     def create_invoice(storage_key)
-      response = request.make(:post, 'invoices', {storage_key: storage_key})
+      #
+      # FIXME:
+      # Because we updated the create API to take these extra parameters without making them optional,
+      # I'm changing it here because the change required in the backend will break the mobile client.
+      # We need to fix it on the backend and update all the clients.
+      # Until then, this hack will do - kit
+      #
+      stubbed_parameters = {remarks: '', expense_type: '', date: Date.today.iso8601}
+      response = request.make(:post, 'invoices', {storage_key: storage_key}.merge(stubbed_parameters))
       MultiJson.load(response.body)['id']
     end
 
