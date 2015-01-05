6cfd1957efd54d6f17ccc00c893db8d8b40ba131
Smit Shah <who828@gmail.com>
2015-01-05 14:11:30 +0530

    Change the labels for clearer understanding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,17 @@
 module ApplicationHelper
   def sortable(column, title = nil)
+    new_column = convert(column)
     title ||= column.titleize
-    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
-    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    css_class = (new_column == sort_column) ? "current #{sort_direction}" : nil
+    direction = (new_column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+    link_to title, {:sort => new_column, :direction => direction}, {:class => css_class}
+  end
+
+  def convert(column)
+    {"Merchant Name" => "Name",
+      "Expense Date" => "Date",
+      "created_at" => "created_at",
+      "Amount" => "Amount",
+      "Remarks" => "Remarks"}[column]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :invoices
+  resources :expenses, controller: 'invoices', :as => 'invoices'
 
   root 'home#dashboard'
 end
