c03d2922fe5da21e6c249a98b08126060337afb2
Jithu Gopal <jithug87@gmail.com>
2015-01-20 15:49:29 +0530

    circumvent temp bucket upload workflow

--- app/models/kulu_aws.rb
+++ app/models/kulu_aws.rb
@@ -1,6 +1,6 @@
 class KuluAWS
   def s3_bucket
-    @s3_bucket ||= AWS::S3.new.buckets[ENV['KULU_S3_TMP_BUCKET']]
+    @s3_bucket ||= AWS::S3.new.buckets[ENV['KULU_S3_BUCKET']]
   end
 
   def presigned_post
